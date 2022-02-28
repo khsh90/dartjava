c056b58c4f4a13c13c9f07152b681ca5759df059
Khaled Hadidi <khsh400@gmail.com>
2022-02-28 23:48:31 +0300

    finish 33

--- ex32.dart
+++ ex32.dart
@@ -4,14 +4,34 @@
 // Input second integer: 39
 // Expected Output
 
-// 25 != 39                                                                          
-// 25 < 39                                                                           
+// 25 != 39
+// 25 < 39
 // 25 <= 39
 
+import 'dart:io';
 
-void main () {
+import 'dart:math';
 
-  
-    
-  
+void main() {
+  while (true) {
+    int? firstNumber;
+    int? secNumber;
+
+    stdout.write('please enter the first number  : ');
+    var input1 = stdin.readLineSync();
+    if (input1 != null && input1.isNotEmpty) {
+      var num1 = int.tryParse(input1);
+      firstNumber = num1;
+    }
+    stdout.write('please enter the seconed number : ');
+    var input2 = stdin.readLineSync();
+    if (input2 != null && input2.isNotEmpty) {
+      var num2 = int.tryParse(input2);
+      secNumber = num2;
+    }
+    if (firstNumber != null && secNumber != null) {
+      var maxNumber = max(firstNumber, secNumber);
+      print('Max number : $maxNumber');
+    }
+  }
 }
--- ex33.dart
+++ ex33.dart
@@ -1,3 +1,32 @@
-void main () {
-  
+// 33. Write a Java program and compute the sum of the digits of an integer. Go to the editor
+// Input Data:
+// Input an integer: 25
+// Expected Output
+
+// The sum of the digits is: 7
+
+import 'dart:io';
+
+void main() {
+  while (true) {
+    var number;
+    var arrNumbers = <int>[];
+    var total = 0;
+
+    stdout.write('please enter the a number  : ');
+    var input1 = stdin.readLineSync();
+    if (input1 != null && input1.isNotEmpty) {
+      var splitNumber = input1.split("");
+
+      splitNumber.forEach((element) {
+        number = int.tryParse(element);
+        arrNumbers.add(number);
+      });
+
+      for (var singleNumber in arrNumbers) {
+        total = total + singleNumber;
+      }
+      print(total);
+    }
+  }
 }
