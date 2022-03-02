d0197654292c64edd27f7654b2951cb5fb3d31ff
Khaled Hadidi <khsh400@gmail.com>
2022-03-02 16:43:25 +0300

    ex47

--- ex44.dart
+++ ex44.dart
@@ -1,3 +1,18 @@
-void main () {
-  
+// 44. Write a Java program that accepts an integer (n) and computes the value of n+nn+nnn. Go to the editor
+// Sample Output:
+
+// Input number: 5
+// 5 + 55  + 555
+
+import 'dart:io';
+
+void main() {
+  stdout.write('Please enter a number: ');
+  var input = stdin.readLineSync();
+
+  if (input != null && input.isNotEmpty) {
+    //  var userinput = int.tryParse(input);
+
+    print('$input+$input$input+$input$input$input');
+  }
 }
---
+++ ex46.dart
@@ -0,0 +1,7 @@
+// 46. Write a Java program to display the system time. Go to the editor
+// Sample Output:
+
+// Current Date time: Fri Jun 16 14:17:40 IST 2017
+// Click me to see the solution
+
+void main() {}
