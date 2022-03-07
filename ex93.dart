943139f39da1849ee14e543083de4d4c30403ba0
Khaled Hadidi <khsh400@gmail.com>
2022-03-07 17:00:35 +0300

    ex94

--- ex93.dart
+++ ex93.dart
@@ -1,9 +1,19 @@
-// // 93. Write a Java program to test if an array of
+// 93. Write a Java program to test if an array of
 //integers contains an element 10 next to 10 or an element 20 next to 20,
 //but not both. Go to the editor
 // Click me to see the solution
 
 void main() {
-  var arr = [1, 2, 6, 5, 10, 10, 15];
-  for (var i = 0; i < arr.length; i++) {}
+  var matchedArr = [];
+
+  var arr = [1, 2, 6, 5, 20, 20, 15, 10, 10, 60, 60, 70, 90, 80, 80];
+
+  for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i] == arr[i + 1]) {
+      matchedArr.add(arr[i]);
+      break;
+    }
+  }
+
+  print('The matched numbers is ${matchedArr.join()}');
 }
---
+++ ex94.dart
@@ -0,0 +1,7 @@
+// 94. Write a Java program to rearrange all the elements of a given array of integers so that
+// all the odd numbers come before all the even numbers. Go to the editor
+// Click me to see the solution
+
+void main() {
+  var numbers = [];
+}
