c6ffcd2d364ed46302f895a86309985583da33b3
Khaled Hadidi <khsh400@gmail.com>
2022-03-14 11:20:34 +0300

    ex153

---
+++ part2/ex153.dart
@@ -0,0 +1,7 @@
+// 153. Write a Java program that accepts two double variables and 
+//test if both strictly between 0 and 1 and false otherwise. Go to the editor
+
+// Sample Output:
+// Input first number: 5
+// Input second number: 1
+// false
