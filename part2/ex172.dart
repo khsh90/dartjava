e337154ab41cb6347fe7cbcce529f7ecdcfd1d84
Khaled Hadidi <khsh400@gmail.com>
2022-03-17 17:42:53 +0300

    finish ex172

--- part2/ex172.dart
+++ part2/ex172.dart
@@ -2,18 +2,23 @@
 //integers that are smaller than the integer of another given array of integers.
 
 void main() {
-  var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-  var inquiryArry = [1, 4];
-  var lenght;
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  List<int> inquiryArry = [1, 4, 8];
+  int lenght = 0;
+  List<int> tempResult = [];
 
-  var testArr = [];
+  List<int> result = [];
+
+  List<int> testArr = [];
   for (var i = 0; i < inquiryArry.length; i++) {
     for (var j = 0; j < numbers.length; j++) {
       if (numbers[j] <= inquiryArry[i]) {
-        testArr.add(numbers[j]);
-        lenght = testArr.length - 1;
+        lenght = numbers[j];
       }
     }
-    print(lenght);
+
+    tempResult.add(lenght - 1);
+    result = [...tempResult];
   }
+  print(result);
 }
