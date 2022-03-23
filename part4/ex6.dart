432a2d165c4784a8824ea09aff3268fa891852d5
Khaled Hadidi <khsh400@gmail.com>
2022-03-24 01:01:37 +0300

    ex7

--- part4/ex6.dart
+++ part4/ex6.dart
@@ -1,5 +1,46 @@
-void main() {
-  int age = 5;
+// 6. Write a Java program that reads in two floating-point numbers and tests whether they are the same up to three decimal places. Go to the editor
 
-  assert(10 < age);
+// Test Data
+// Input floating-point number: 25.586
+// Input floating-point another number: 25.589
+// Expected Output :
+// They are different
+
+import 'dart:io';
+
+void main() {
+  while (true) {
+    stdout.write('Enter Number1: ');
+    var userInput1 = stdin.readLineSync();
+
+    stdout.write('Enter Number2: ');
+    var userInput2 = stdin.readLineSync();
+
+    if (userInput1 != null &&
+        userInput2 != null &&
+        userInput1.isNotEmpty &&
+        userInput2.isNotEmpty) {
+      compareTwoNumbers(userInput1: userInput1, userInput2: userInput2);
+    } else {
+      print('Please enter a number');
+    }
+  }
+}
+
+void compareTwoNumbers({required userInput1, required userInput2}) {
+  try {
+    var intUserInput1 = double.tryParse(userInput1);
+    var intUserInput2 = double.tryParse(userInput2);
+    if (intUserInput1 != null &&
+        intUserInput2 != null &&
+        intUserInput1 == intUserInput2) {
+      print('The numbers are mateched');
+    } else if (intUserInput1 != null &&
+        intUserInput2 != null &&
+        intUserInput1 != intUserInput2) {
+      print('The numbers are not matched ');
+    }
+  } catch (_) {
+    print('Only number are allowed');
+  }
 }
---
+++ part4/ex7.dart
@@ -0,0 +1,46 @@
+// 7. Write a Java program to find the number of days in a month. Go to the editor
+
+// Test Data
+// Input a month number: 2
+// Input a year: 2016
+// Expected Output :
+// February 2016 has 29 days
+
+import 'dart:io';
+
+import 'package:date_utils/date_utils.dart';
+
+void main() {
+  while (true) {
+    stdout.write('Please enter a month as number like jan 1 , feb 2 etc... : ');
+    var userInput1 = stdin.readLineSync();
+
+    stdout.write('Please enter a year ');
+    var userInput2 = stdin.readLineSync();
+
+    if (userInput1 != null &&
+        userInput2 != null &&
+        userInput1.isNotEmpty &&
+        userInput2.isNotEmpty) {
+      getNumberOfDays(month: userInput1, year: userInput2);
+    } else {
+      print('Please enter an input');
+    }
+  }
+}
+
+void getNumberOfDays({required String month, required String year}) {
+  var intMonth = int.tryParse(month);
+  var intYear = int.tryParse(year);
+
+  try {
+    if (intMonth != null && intYear != null) {
+      var numberOfMonth = DateUtils.lastDayOfMonth(DateTime(intYear, intMonth));
+
+      print(
+          'The number of enered month based on year $intYear :$numberOfMonth ');
+    }
+  } catch (_) {
+    print('Please enter a valid year and month ');
+  }
+}
